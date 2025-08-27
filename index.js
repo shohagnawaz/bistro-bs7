0c627b82cdfebf47eed4761de0364ebbd2bc6973
shohagnawaz <snawazdigital@gmail.com>
2025-08-27 19:08:30 +0600

    abc

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
     app.get("/menu", async(req, res) => {
       const result = await menuCollection.find().toArray();
@@ -38,6 +39,13 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
+
+    // carts collection
+    app.post("/carts", async(req, res) => {
+        const cartItem = req.body;
+        const result = await cartCollection.insertOne(cartItem);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -56,3 +64,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Bistro boss is sitting on port ${port}`)
 })
+
+/*
+------------------------
+    NAMING CONVENTION  
+------------------------
+1. app.get("/users") = get all users
+2. app.get("/users/:id") = get specific id user
+3. app.post("/users") = create users
+4. app.put("/users/:id") = something specific user
+5. app.patch("/users/:id") = something specific user
+6. app.delete("/users/:id") =  
+
+*/ 
