1d30ceeb94ecb2e292fcc20a3e0f96f04fe6744b
shohagnawaz <snawazdigital@gmail.com>
2025-08-28 18:54:01 +0600

    appCart

--- index.js
+++ index.js
@@ -41,11 +41,18 @@ async function run() {
     });
 
     // carts collection
+    app.get("/carts", async(req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+    });
+
     app.post("/carts", async(req, res) => {
         const cartItem = req.body;
         const result = await cartCollection.insertOne(cartItem);
         res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
